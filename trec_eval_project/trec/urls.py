48dd4a78353c90e99c888e65f843e9bd444bba0a
“JosephOHagan” <josephaohagan@gmail.com>
2016-03-12 21:39:19 +0000

    Created trec/urls.py as missing / required.

---
+++ trec_eval_project/trec/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, url
+from trec import views
+
+urlpatterns = patterns('',
+        url(r'^$', views.home, name='home'),
+        url(r'^about', views.about, name='about'),
+        )
